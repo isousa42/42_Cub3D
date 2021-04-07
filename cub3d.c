98d79ec113c8e09d64e3655dbfa6e897f2b29d9a
SeniAlex <76601369+SeniAlex@users.noreply.github.com>
2021-04-07 18:47:38 +0100

    BIG pinky cube in center of whity window

--- cub3d.c
+++ cub3d.c
@@ -4,35 +4,47 @@
 #include <stdio.h>
 #include <mlx.h>
 
-typedef struct	s_fl
-{
-	int		pixel_1;
-	int		pixel_2;
-	int		pixel_3;
-	int		pixel_4;
-    int		pixel_5;
-	int		pixel_6;
-	int		pixel_7;
-	int		pixel_8;
-
-}				t_flags;
-
 int main()
 {
     void    *mlx_ptr;
     void    *win_ptr;
-    struct s_fl    pixels;
+    int     y;
+    int     x;
+    int     max_x;
+    int     max_y;
+    int     pixel;
+    int pos_x;
+    int pos_y;
+
+    y = 0;
+    x = 0;
+    max_x = 1000;
+    max_y = 1000;
+    pos_x = max_x / 2 - 50;
+    pos_y = max_y / 2 - 50;
 
     mlx_ptr = mlx_init();
-    win_ptr = mlx_new_window(mlx_ptr, 500, 500, "Cub3D");
-    pixels.pixel_1 = mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0xff007f);
-    pixels.pixel_2 = mlx_pixel_put(mlx_ptr, win_ptr, 251, 250, 0xff007f);
-    pixels.pixel_3 = mlx_pixel_put(mlx_ptr, win_ptr, 250, 251, 0xff007f);
-    pixels.pixel_4 = mlx_pixel_put(mlx_ptr, win_ptr, 251, 251, 0xff007f);
-    pixels.pixel_5 = mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0xff007f);
-    pixels.pixel_6 = mlx_pixel_put(mlx_ptr, win_ptr, 251, 250, 0xff007f);
-    pixels.pixel_7 = mlx_pixel_put(mlx_ptr, win_ptr, 250, 251, 0xff007f);
-    pixels.pixel_8 = mlx_pixel_put(mlx_ptr, win_ptr, 251, 251, 0xff007f);
+    win_ptr = mlx_new_window(mlx_ptr, max_x, max_y, "Cub3D");
+    while (x <= max_x)
+    {
+        while (y <= max_y)
+        {
+            pixel = mlx_pixel_put(mlx_ptr, win_ptr, x, y, 0xffffff);
+            y++;
+        }
+        x++;
+        y = 0;
+    }
+    while (pos_x <= (max_x / 2) + 50)
+    {
+        while (pos_y <= (max_y / 2) + 50)
+        {
+            pixel = mlx_pixel_put(mlx_ptr, win_ptr, pos_x, pos_y, 0xff007f);
+            pos_y++;
+        }
+        pos_x++;
+        pos_y = max_y / 2 - 50;
+    }
     mlx_loop(mlx_ptr);
-    
 }
+
