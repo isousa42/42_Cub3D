b9b8545521b30b4b7bf1775e090d7ff419ac0fb6
SeniAlex <76601369+SeniAlex@users.noreply.github.com>
2021-04-09 18:10:53 +0100

    I understood that I just dont need this.

--- cub3d.c
+++ cub3d.c
@@ -6,7 +6,7 @@
 typedef struct s_player
 {
     void    *player;
-    char    *addr;
+    int    *addr;
     int     bits_per_pixel;
     int     line_lenght;
     int     endian;
@@ -31,80 +31,82 @@ void    clear_pixels(t_player *player, int x, int y, int lim_x, int lim_y, int c
     }
 }
 
-void    put_pixel(t_player *player, int x, int y, int color)
+void	draw_rectangle(t_player *player, int x, int y)
 {
-    char *offset;
+	int i;
+	int j;
 
-    offset = player->addr + (y * player->line_lenght + x * (player->bits_per_pixel / 8));
-    *(unsigned int*)offset = color;
+	i = 0;
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{  
+			mlx_pixel_put(player->mlx_ptr, player->win_ptr, player->pos_x + i, player->pos_y + j, 0xffffff);
+			j++;
+		}
+		i++;
+	}
 }
 
-void    create_player(t_player *player)
-{
-    player->pos_x = 225;
-    player->pos_y = 225;
-    int x = 0;
-    int y = 0;
+// void    put_pixel(t_player *player, int x, int y, int color)
+// {
+//     char *offset;
 
-    player->player = mlx_new_image(player->mlx_ptr, 50, 50);
-    player->addr = mlx_get_data_addr(player->player, &player->bits_per_pixel, &player->line_lenght, &player->endian);
-    while (x <= 50)
-    {
-        while (y <= 50)
-        {
-            put_pixel(player, x, y, 0xff007f);
-            y++;
-        }
-        x++;
-        y = 0;
-    }
-    mlx_put_image_to_window(player->mlx_ptr, player->win_ptr, player->player, player->pos_x, player->pos_y);
-}
+//     offset = player->addr + (y * player->line_lenght + x * (player->bits_per_pixel / 8));
+//     *(unsigned int*)offset = color;
+// }
+
+// void    create_player(t_player *player)
+// {
+//     int x = 0;
+//     int y = 0;
+
+//     while (x < 8)
+//     {
+//         y = 0;
+//         while (y < 8)
+//         {
+//             put_pixel(player, player->pos_x+ x, player->pos_y + y, 0xff007f);
+//             y++;
+//         }
+//         x++;
+//     }
 
 int     go_down(t_player *player)
 {
     
-    int x = player->pos_x;
-    int y = player->pos_y;
-    clear_pixels(player, x, y, 500, 500, 0xffffff);
     player->pos_y += 10;
-    mlx_put_image_to_window(player->mlx_ptr, player->win_ptr, player->player, player->pos_x, player->pos_y);
-
     return 0;
 }
 
 int     go_up(t_player *player)
 {
     
-    int x = player->pos_x;
-    int y = player->pos_y;
-    clear_pixels(player, x, y, 500, 500, 0xffffff);
     player->pos_y -= 10;
-    mlx_put_image_to_window(player->mlx_ptr, player->win_ptr, player->player, player->pos_x, player->pos_y);
-
     return 0;
 }
 
 int     go_left(t_player *player)
 {
     
-    int x = player->pos_x;
-    int y = player->pos_y;
-    clear_pixels(player, x, y, 500, 500, 0xffffff);
     player->pos_x -= 10;
-    mlx_put_image_to_window(player->mlx_ptr, player->win_ptr, player->player, player->pos_x, player->pos_y);
 
+    return 0;
+}
+int     main_loop(t_player *player)
+{
+    draw_rectangle(player, player->pos_x, player->pos_y);
+    // clear_pixels(player, 0, 0, 500, 500, 0xffffff);
+    // mlx_put_image_to_window(player->mlx_ptr, player->win_ptr, player->player, 0, 0);
+    
     return 0;
 }
 
 int     go_right(t_player *player)
 {
     
-    int x = player->pos_x;
-    int y = player->pos_y;
-    clear_pixels(player, x, y, 500, 500, 0xffffff);
     player->pos_x += 10;
-    mlx_put_image_to_window(player->mlx_ptr, player->win_ptr, player->player, player->pos_x, player->pos_y);
 
     return 0;
 }
@@ -128,9 +130,16 @@ int main()
 
     player.mlx_ptr = mlx_init();
     player.win_ptr = mlx_new_window(player.mlx_ptr, 500, 500, "Cub3D");
-    clear_pixels(&player, 0, 0, 500, 500, 0xffffff);
-    create_player(&player);
-    mlx_key_hook(player.win_ptr, key_hook, &player);
+    // clear_pixels(&player, 0, 0, 500, 500, 0xffffff);
+    // player.player = mlx_new_image(player.mlx_ptr, 500, 500);
+    // player.addr = (int *)mlx_get_data_addr(player.player, &player.bits_per_pixel, &player.line_lenght, &player.endian);
+    // mlx_put_image_to_window(player.mlx_ptr, player.win_ptr, player.player, 0, 0); 
+    player.pos_x = 100;
+    player.pos_y = 150;
+    mlx_loop_hook(player.mlx_ptr, &main_loop, &player);
+    mlx_hook(player.win_ptr, 2, 0, &key_hook, &player);
+    
+
     mlx_loop(player.mlx_ptr);
 }
 
